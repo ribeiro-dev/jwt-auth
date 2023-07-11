d6354300e8e3f8df39e28f36c6123f8298e1b6f3
ribeiro-dev <romilson.2r@gmail.com>
2023-07-10 22:59:23 -0300

    add: login route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import express from "express";
 import "dotenv/config"; // import and invoke
 
-import UsersController from 'src/UsersController';
-
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 
+app.use(express.json())
+
+const users = [
+    {
+        email: 'john.doe@email.com',
+        password: '123@@abc'
+    },
+    {
+        email: 'bill@email.com',
+        password: '123@@abc'
+    }
+];
+
+
+// Routes
 app.get('/', (req, res) => {
     res.json({message: 'Hello World!'});
+})
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    const user = users.find(user => user.email == email);
+
+    if (!user) {
+        return res.status(404).json({ message: 'Email não encontrado' });
+    };
+
+    if (user.email != email || user.password != password) {
+        return res.status(401).json({ message: 'Email e/ou senha inválidos' });
+    }
+
+    res.json({ message: 'Logged in!' });
 })
 
 app.listen(PORT, () => {
